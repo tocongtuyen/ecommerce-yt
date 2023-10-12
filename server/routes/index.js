d35be6876fb5a10aed0c0a7deb6a980769dff064
Tô Công Tuyển <tocongtuyen@gmail.com>
2023-10-13 02:36:28 +0700

    #1 - handle error

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,7 +1,11 @@
+const { notFound, errorHandler } = require("../middlewares/errorHandler")
 const userRouter = require("./user")
 
 const initRoutes = (app) => {
     app.use("/api/user",userRouter)
+
+    app.use(notFound)
+    app.use(errorHandler)
 }
 
 module.exports = initRoutes
